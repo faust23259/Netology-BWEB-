a6e461fa141a7cbf545ea84762357d9bcb2e8a12
Nikita Mishin <nikita.mishin.91@list.ru>
2025-04-23 10:38:06 +0300

    hide api & secret key

--- homework-1-http/config_dadata.php
+++ homework-1-http/config_dadata.php
@@ -1,3 +1,3 @@
 <?php
-define('API_KEY', 'a90d46bbb8aa9f858a11cf21c95d30f9c760b7bf');
-define('SECRET_KEY', 'c9b29cda3e38030b1a10b57ee91824ed5056f0db');
+define('API_KEY', '...');  //insert your data
+define('SECRET_KEY', '...');  //insert your data
